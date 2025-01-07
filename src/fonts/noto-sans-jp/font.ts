fc9c9d835512c8760ed0a8b73c27d5abdc84ac8d
nyarla <nyarla@kalaclista.com>
2025-01-07 12:14:38 +0900

    refactor(font): refactor type name of fonts

--- src/fonts/noto-sans-jp/font.ts
+++ src/fonts/noto-sans-jp/font.ts
@@ -1,10 +1,9 @@
-import type { FontWeight } from "satori";
-import type { FontConfig } from "../../font";
+import type { Font, FontWeight } from "../../font";
 import type { License } from "../../license";
 
 const { file } = Bun;
 
-const weights = {
+const weights: Record<FontWeight, string> = {
   100: "Thin",
   200: "ExtraLight",
   300: "Light",
@@ -14,34 +13,52 @@ const weights = {
   700: "Bold",
   800: "ExtraBold",
   900: "Black",
+} as const;
+
+const name = "Noto Sans JP";
+const lang = "ja-JP";
+
+const summary =
+  "Noto Sans JP is an unmodulated (“sans serif”) design for the Japanese language and other languages used in Japan";
+
+const homepageUrl = new URL(
+  "https://fonts.google.com/noto/specimen/Noto+Sans+JP",
+);
+const licenses = [
+  {
+    spdx: "OFL-1.1",
+    copyrights: [
+      "(c) 2014-2021 Adobe (http://www.adobe.com/), with Reserved Font Name 'Source'.",
+    ],
+    licenseUrl: new URL(
+      "https://github.com/google/fonts/blob/main/ofl/notosansjp/OFL.txt",
+    ),
+  },
+];
+
+const meta = {
+  name,
+  lang,
+  summary,
+  homepageUrl,
+  licenses,
+} as const;
+
+const fn = (weight?: FontWeight): URL => {
+  if (!weight) {
+    return new URL("./font-Regular.ttf", import.meta.url);
+  }
+
+  if (!(weight in weights)) {
+    throw new Error(
+      `This font does not support to this weight: Noto Sans JP: ${weight}`,
+    );
+  }
+
+  return new URL(`./font-${weights[weight]}.ttf`, import.meta.url);
 };
 
-const spec = {
-  name: "Noto Sans JP",
-  lang: "ja-JP",
-  summary:
-    "Noto Sans JP is an unmodulated (“sans serif”) design for the Japanese language and other languages used in Japan",
-  homepageUrl: "https://fonts.google.com/noto/specimen/Noto+Sans+JP",
-  licenses: [
-    {
-      spdx: "OFL-1.1",
-      copyrights: [
-        `(c) 2014-2021 Adobe (http://www.adobe.com/), with Reserved Font Name 'Source'.`,
-      ],
-      licenseUrl: new URL(
-        "https://github.com/google/fonts/blob/main/ofl/notosansjp/OFL.txt",
-      ),
-    },
-  ] as License[],
-};
+const load = (weight?: FontWeight): Promise<ArrayBuffer> =>
+  file(fn(weight)).arrayBuffer();
 
-const load = (weight?: FontWeight) => {
-  const fn = new URL(
-    weight ? `./font-${weights[weight]}.ttf` : `./font-Regular.ttf`,
-    import.meta.url,
-  );
-
-  return file(fn).arrayBuffer();
-};
-
-export const NotoSansJP: FontConfig = { spec, load } as const;
+export const NotoSansJP: Font = { meta, load } as const;
--- test/fonts/NotoSansJP.test.ts
+++ test/fonts/NotoSansJP.test.ts
@@ -3,12 +3,14 @@ import { NotoSansJP as config } from "../../src/fonts/noto-sans-jp/font";
 
 describe("NotoSansJP", () => {
   test("spec", () => {
-    const { name, lang, summary, homepageUrl, licenses } = config.spec;
+    const { name, lang, summary, homepageUrl, licenses } = config.meta;
 
     expect(name).toBe("Noto Sans JP");
     expect(lang).toBe("ja-JP");
     expect(summary).toBeString();
-    expect(homepageUrl).toBeString();
+    expect(homepageUrl).toEqual(
+      new URL("https://fonts.google.com/noto/specimen/Noto+Sans+JP"),
+    );
     expect(licenses[0].spdx).toBe("OFL-1.1");
     expect(licenses[0].copyrights[0]).toBeString();
     expect(licenses[0].licenseUrl).toEqual(
